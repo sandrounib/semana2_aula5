46741f5d5af933386bd077df155dab5713b07238
FIC <sandrounib@hotmail.com>
2017-10-11 19:23:13 -0300

    Perguntas e Respostas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace semana2_aula5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] perguntas = new string[10];
+            string[] respostas = new string[10];
+            string[] Perguntas = File.ReadAllLines(@"C:\Users\FIC\Desktop\semana2_aula5\Perguntas.txt");
+            string[] Respostas = new string[perguntas.Length];
+
+            for (int i = 0; i < perguntas.Length; i ++)
+            {
+                Console.WriteLine("Qual é o seu/sua" + perguntas[i]);
+                respostas[i] = Console.ReadLine();
+            }
+        }
+    }
+}
