7220e52c148c88470215e4a75ac58d92955679cb
FIC <sandrounib@hotmail.com>
2017-10-11 20:21:53 -0300

    Perguntas e Respostas em Arquivo Excel

--- Program.cs
+++ Program.cs
@@ -7,16 +7,25 @@ namespace semana2_aula5
     {
         static void Main(string[] args)
         {
-            string[] perguntas = new string[10];
-            string[] respostas = new string[10];
-            string[] Perguntas = File.ReadAllLines(@"C:\Users\FIC\Desktop\semana2_aula5\Perguntas.txt");
-            string[] Respostas = new string[perguntas.Length];
+            //string[] perguntas = new string[10];            
+            string[] perguntas = File.ReadAllLines(@"C:\Users\FIC\Desktop\semana2_aula5\Perguntas.txt");
+            //string[] respostas = new string[];
+            string[] respostas = new string[perguntas.Length];            
+
 
             for (int i = 0; i < perguntas.Length; i ++)
             {
-                Console.WriteLine("Qual é o seu/sua" + perguntas[i]);
+                Console.WriteLine("Qual é o seu/sua: " + perguntas[i]);
                 respostas[i] = Console.ReadLine();
             }
+
+
+            StreamWriter sw = new StreamWriter (@"C:\Users\FIC\Desktop\semana2_aula5\Respostas.csv");
+            for (int i = 0; i < respostas.Length; i ++)
+            {
+                sw.Write(respostas[i] + ";");                
+            }
+            sw.Close();
         }
     }
 }
