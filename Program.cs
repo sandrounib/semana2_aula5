2c871fff5118e050cbee6b3b83d6354440f3058f
FIC <sandrounib@hotmail.com>
2017-10-11 20:29:11 -0300

    .

--- Program.cs
+++ Program.cs
@@ -6,20 +6,14 @@ namespace semana2_aula5
     class Program
     {
         static void Main(string[] args)
-        {
-            //string[] perguntas = new string[10];            
-            string[] perguntas = File.ReadAllLines(@"C:\Users\FIC\Desktop\semana2_aula5\Perguntas.txt");
-            //string[] respostas = new string[];
-            string[] respostas = new string[perguntas.Length];            
-
-
+        {                        
+            string[] perguntas = File.ReadAllLines(@"C:\Users\FIC\Desktop\semana2_aula5\Perguntas.txt");            
+            string[] respostas = new string[perguntas.Length];         
             for (int i = 0; i < perguntas.Length; i ++)
             {
                 Console.WriteLine("Qual é o seu/sua: " + perguntas[i]);
                 respostas[i] = Console.ReadLine();
             }
-
-
             StreamWriter sw = new StreamWriter (@"C:\Users\FIC\Desktop\semana2_aula5\Respostas.csv");
             for (int i = 0; i < respostas.Length; i ++)
             {
